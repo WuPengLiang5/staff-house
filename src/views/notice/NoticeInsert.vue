67ce68f83bfd85a7e608eba27ac925089cba81ee
lzj <1046175474@qq.com>
2021-06-18 11:50:50 +0800

    update src/views/notice/NoticeInsert.vue.

--- src/views/notice/NoticeInsert.vue
+++ src/views/notice/NoticeInsert.vue
@@ -3,7 +3,7 @@
     <div>
         <el-breadcrumb separator-class="el-icon-arrow-right" id="head">
             <el-breadcrumb-item>当前位置：公告管理</el-breadcrumb-item>
-            <el-breadcrumb-item>公告查询</el-breadcrumb-item>
+            <el-breadcrumb-item>添加公告</el-breadcrumb-item>
         </el-breadcrumb>
         <div>
         <el-form  label-width="220px" style="margin-top: 50px;">
