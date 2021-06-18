61a2766c1573b09e103a77e6f19daa410350c0a7
李中镜 <1046175474@qq.com>
2021-06-18 11:35:04 +0800

    公告管理

--- src/views/notice/NoticeInsert.vue
+++ src/views/notice/NoticeInsert.vue
@@ -1,13 +1,71 @@
 <template>
-    <div>添加公告</div>
+
+    <div>
+        <el-breadcrumb separator-class="el-icon-arrow-right" id="head">
+            <el-breadcrumb-item>当前位置：公告管理</el-breadcrumb-item>
+            <el-breadcrumb-item>公告查询</el-breadcrumb-item>
+        </el-breadcrumb>
+        <div>
+        <el-form  label-width="220px" style="margin-top: 50px;">
+            <el-form-item label="公告名称" style="margin-right: 120px" >
+                <el-input v-model="title"></el-input>
+            </el-form-item>
+            <el-divider></el-divider>
+            <el-form-item label="公告内容" style="margin-right: 120px" >
+                <el-input
+                        v-model="text"
+                        type="textarea"
+                        :autosize="{ minRows: 10, maxRows: 10}"
+                        placeholder="请输入内容"
+                        show-word-limit="true"
+                >
+                </el-input>
+            </el-form-item>
+            <el-divider></el-divider>
+            <el-form-item style="margin-right: 120px">
+                <el-button type="primary" @click="add">添加</el-button>
+                <el-button type="primary" @click="rebuild">重置</el-button>
+            </el-form-item>
+        </el-form>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "NoticeInsert"
+        name: "NoticeInsert",
+        data(){
+            return{
+                title:"",
+                text:""
+            }
+        },
+        methods:{
+            rebuild(){
+                this.title="";
+                this.text="";
+            },
+            add(){
+                this.$message({
+                    message: '添加成功',
+                    type: 'success'
+                });
+                this.title="";
+                this.text="";
+            }
+        }
     }
 </script>
 
 <style scoped>
+#head{
+    padding-left: 10px;
+    margin-left:-20px;
+    height: 40px;
+    background-color: #82ecff;
+    padding-top: 25px;
+    width: 1400px;
+    margin-top: -25px;
 
+}
 </style>
